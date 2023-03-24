728f6834f951d0febb60ef75284d82709b0e3e02
mfaeezshabbir <faizytech1st@gmail.com>
2023-03-24 22:32:44 +0500

    24 March

--- .history/src/Components/VideoCard_20230323133058.jsx
+++ .history/src/Components/VideoCard_20230323133058.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-import { demoThumbnailUrl, demoVideoUrl, demoChannelUrl, demoChannelTitle } from '../utils/constant';
+import { demoThumbnailUrl, demoVideoUrl , demoChannelUrl, demoChannelTitle } from '../utils/constant';
 function VideoCard() {
   return (
     <div>
